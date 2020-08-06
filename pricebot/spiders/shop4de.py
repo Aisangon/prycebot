1285ca1a7b5cd756a2799ebcb9159cf63341bfa7
Ainel <ainelainel@hotmail.com>
2020-08-06 22:27:37 +0200

    scrape all products via pagination

--- pricebot/spiders/shop4de.py
+++ pricebot/spiders/shop4de.py
@@ -3,10 +3,8 @@ import scrapy
 
 class Shop4DeSpider(scrapy.Spider):
     name = "shop4de"
-
-    start_urls = [
-        'https://www.shop4de.com/nintendo-switch-games'
-    ]
+    start_urls = ['https://www.shop4de.com/nintendo-switch-games']
+    base_url = "https://www.shop4de.com/nintendo-switch-games/page-"
 
     def parse(self, response):
         for product in response.css("div.product_box"):
@@ -14,3 +12,9 @@ class Shop4DeSpider(scrapy.Spider):
                 'name': product.css("div.top_half h3.title a::text").get(),
                 'price': product.css("div.bot_half b.price::text").get()
             }
+
+        current_page = response.css('a#load_more::attr(data-page)').get()
+        if current_page is not None:
+            next_page = str(int(current_page) + 1)
+            final_url = self.base_url + next_page
+            yield scrapy.Request(final_url, self.parse)
