ca8071d28f353df02e9a4fd9e1a063e52ac61b9f
Ainel <ainelainel@hotmail.com>
2020-08-12 22:21:14 +0200

    set up item pipeline and send formatted html as message - #5

--- pricebot/spiders/quotes_spider.py
+++ pricebot/spiders/quotes_spider.py
@@ -1,6 +1,9 @@
 import scrapy
 from scrapy import signals
 from pubsub import pub
+from pricebot.items import Book
+import json
+from itemadapter import ItemAdapter
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
@@ -12,9 +15,12 @@ class QuotesSpider(scrapy.Spider):
 
     def parse(self, response):
         for quote in response.css('div.quote'):
-            yield {
-                # 'text': quote.css('span.text::text').get(),
-                'author': quote.css('small.author::text').get()
-                # 'tags': quote.css('div.tags a.tag::text').getall(),
-            }
+            book = Book()
+            book['author'] = quote.css('small.author::text').get()
+            yield ItemAdapter(book).asdict()
     
+            # yield {
+            #     # 'text': quote.css('span.text::text').get(),
+            #     'author': quote.css('small.author::text').get()
+            #     # 'tags': quote.css('div.tags a.tag::text').getall(),
+            # }
